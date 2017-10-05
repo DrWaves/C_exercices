5f06d7c2cc144b62db1c8c7bf8bcc5b3e908c569
DrWaves <elfani.mehdi@gmail.com>
2017-10-05 12:05:05 +0200

    Pass by copy

---
+++ 9_Functions/exo_92.c
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+void function (int a);
+
+int main(int argc, char const *argv[]) {
+
+  int number=atoi(argv[1]);
+  printf(" Address of the memory in main() %p \n", &number );
+function(number);
+
+
+  return 0;
+}
+
+void function (int a ) {
+  printf(" Address of the memory in function() %p \n", &a);
+
+
+}
